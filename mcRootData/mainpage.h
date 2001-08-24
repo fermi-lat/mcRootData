2f8bc5f2092fb952c3d659e3916a178ccbb9b431
igable <>
2001-08-24 22:54:56 +0000

    Adding mainpage.

---
+++ mcRootData/mainpage.h
@@ -0,0 +1,19 @@
+// Mainpage for doxygen
+
+/*! \mainpage package mcRootData
+
+  This package holds the root parallels of the GlastEvent MonteCarlo classes.
+  RMcEvent holds an array of RMcVertex so that the tree of particles can be 
+  accessed through a TObjArray.
+
+  This package still needs some work.
+
+
+  <hr>
+  \section notes release notes
+  \include release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
