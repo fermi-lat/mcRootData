a790d73d185e57ba0c6490aefc8f6a6f3a0d7570
heather <>
2002-04-23 14:29:31 +0000

    add new test routine

---
+++ src/test/leaktest.cxx
@@ -0,0 +1,74 @@
+// Test routine to make sure there are no memory leaks when creating
+// and deleting mcRootData objects.
+// To Run:
+// 1) Make sure mcRootData.dll is in your ROOT library path
+//    a) This can be done by either modifying your local .rootrc file 
+//       Unix.*.Root.DynamicPath:    $(ROOTANALYSIS)/lib
+//       WinNT.*.Root.DynamicPath:   $(ROOTANALYSIS)/lib
+//   OR
+//    b) Copy mcRootData.so (or .dll) into the directory from where you start ROOT.
+// 2) Start ROOT
+// 3) At the ROOT prompt, type:  ".x leaktest.cxx" 
+
+{
+    UInt_t numEvents = 500;
+    UInt_t numParticles = 100;
+    UInt_t runNum = 1;
+
+    gObjectTable->Print();
+    
+    gSystem->Load("mcRootData.dll");
+    TFile *f =  new TFile("mc.root", "RECREATE");
+    TTree *t = new TTree("Mc", "Mc");
+    McEvent *ev = new McEvent();
+    t->Branch("McEvent", "McEvent", &ev, 64000, 1);
+    
+    gObjectTable->Print();
+    
+    VolumeIdentifier id;
+
+    Int_t ievent, ipart;
+    for (ievent = 0; ievent < numEvents; ievent++) {
+
+        ev->initialize(ievent, runNum);
+
+        for (ipart = 0; ipart < numParticles; ipart ++) {
+            McParticle *mcPart = new McParticle();
+            Float_t rand = gRandom->Rndm();
+            TLorentzVector initMom(1., 2., 3.);
+            TLorentzVector finalMom(ipart*rand+ievent, rand*ievent+ipart, 1., 2.);
+            TVector3 finalPos(ipart*rand, ipart*rand, ipart*rand);
+            mcPart->initialize(mcPart, 0, 0, initMom, finalMom, finalPos);
+            ev->addMcParticle(mcPart);
+            if (ipart % 2) {
+                McPositionHit *posHit = new McPositionHit();
+                TVector3 entry(1., 1., 1.);
+                TVector3 exit(0., 0.5, 0.1);
+                id.initialize(0, 1, 1);
+                posHit->initialize(rand, id, entry, exit, mcPart, 0, rand*0.1, rand*0.4, 0);
+                ev->addMcPositionHit(posHit);
+            } else {
+                McIntegratingHit *intHit = new McIntegratingHit();
+                TVector3 mom1(rand*0.1, rand*0.2, rand*0.3);
+                TVector3 mom2(2., 2., 2.);
+                id.initialize(0, 0, 0);
+                intHit->initialize(id, rand, mom1, mom2);
+                ev->addMcIntegratingHit(intHit);
+            }
+        }
+        t->Fill();
+        ev->Clear();
+    }
+    
+    delete ev;
+    
+    printf("Here is the object table after creating events, storing them to file and deleting the objects\n");
+    gObjectTable->Print();
+    
+    f->Write();
+    f->Close();
+    delete f;
+    
+}
+
+
