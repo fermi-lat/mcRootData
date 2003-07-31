40b7d66227b423094632d06c796a4796d7c99971
berthon <>
2003-07-31 12:11:40 +0000

    increased limit for positionhits

--- src/McEvent.cxx
+++ src/McEvent.cxx
@@ -66,7 +66,7 @@ void McEvent::Clear(Option_t *option) {
     
     m_eventId = 0;
     m_runId = 0;
-    const Int_t ndpos = 20000;
+    const Int_t ndpos = 30000;
     const Int_t nd = 10000;
     static Int_t limit=100;
     static Int_t indpart=0;
