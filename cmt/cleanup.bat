4ec731837a99b5828da1cc25aabff6bfac9ba8f0
burnett <>
2004-06-17 04:51:51 +0000

    use RootPolicy pattern

--- cmt/cleanup.bat
+++ cmt/cleanup.bat
@@ -1,1 +1,9 @@
+@echo off
+if NOT DEFINED CMTROOT set CMTROOT=\ground\CMT\v1r14p20031120 & set PATH=%CMTROOT%\%CMTBIN%;%PATH% & set CMTBIN=VisualC & if not defined CMTCONFIG set CMTCONFIG=%CMTBIN%
+
+set cmttempfile="%TEMP%\tmpsetup.bat"
+%CMTROOT%\%CMTBIN%\cmt.exe -quiet cleanup -bat  -pack=mcRootData -version=v2r11p12 -path=%~d0%~p0..\..\..   %1 %2 %3 %4 %5 %6 %7 %8 %9 >%cmttempfile%
+if exist %cmttempfile% call %cmttempfile%
+if exist %cmttempfile% del %cmttempfile%
+set cmttempfile=
 
---
+++ mcRootData/LinkDef.h
@@ -0,0 +1,17 @@
+/** @file LinkDef.h
+    @brief for rootcint
+ $Header$
+
+*/#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class VolumeIdentifier+;
+#pragma link C++ class McParticle+;
+#pragma link C++ class McPositionHit+;
+#pragma link C++ class McIntegratingHit+;
+#pragma link C++ class McEvent+;
+
+#endif
