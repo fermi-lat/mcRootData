e075caa567ffda469e954e9ac48e0e39c88817b5
ecephas <>
2008-10-27 17:05:11 +0000

    Added rootLibs to list of libraries to link against: 	env.Tool('addLibrary', library = env['rootLibs'])

--- mcRootDataLib.py
+++ mcRootDataLib.py
@@ -3,5 +3,6 @@ def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['mcRootData'])
     env.Tool('commonRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
