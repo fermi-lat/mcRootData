3f7bd568de224dd87f1db4d2bdb908a5b0ad8146
jrb <>
2012-08-17 18:38:29 +0000

    patches for Windows no-install-headers

--- mcRootDataLib.py
+++ mcRootDataLib.py
@@ -2,6 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['mcRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'mcRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
